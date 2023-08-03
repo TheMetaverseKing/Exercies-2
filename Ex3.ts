36205ae207d19bb00ed5e218ead6bfa0b51357cf
TheMetaverseKing <139680450+TheMetaverseKing@users.noreply.github.com>
2023-08-03 23:50:45 +0500

    Add files via upload

---
+++ Ex3.ts
@@ -0,0 +1,11 @@
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+
+
+
+console.log(`Tariq Bin Ziyad once said, "Burn the Boats"`)
+let personname:string = "thE meTaVeRsE kINg";
+let lowercasename : string = personname.toLowerCase();
+let uppercasename : string = personname.toUpperCase();
+console.log(lowercasename);
+console.log(uppercasename);
